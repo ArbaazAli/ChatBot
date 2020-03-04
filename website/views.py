790d21168eb1ee2f5a970d378ef43289df433ed3
ArbaazAli <arbajali02@gmail.com>
2020-03-04 21:29:59 +0530

    added chat box

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse, reverse
 from .models import Student
 import base64
 
@@ -12,8 +12,8 @@ def bot(request):
     chatbot = "hello how are you doing!!"
     if request.method == 'POST':
         output = request.POST['input']
-        return render(request, 'chatbot.html', {'output' : output})
-    return render(request, 'chatbot.html', {'user': user, 'chatbot': chatbot,})
+        return render(request, 'chat.html', {'output' : output})
+    return render(request, 'chat.html', {'user': user, 'chatbot': chatbot,})
 
 
 def register(request):
@@ -23,11 +23,10 @@ def register(request):
         student_email = request.POST['student-email']
         student_contact = request.POST['student-contact']
         student_branch = request.POST['student-branch']
-        encoded_password = base64.b64encode(b'{request.POST["student-password"]}')
-        data = Student(student_name = student_name, student_id = student_id, student_contact = student_contact, student_email = student_email, student_branch = student_branch, student_password = encoded_password)
+        student_password = request.POST['student-password']
+        data = Student(student_name = student_name, student_id = student_id, student_contact = student_contact, student_email = student_email, student_branch = student_branch, student_password = student_password)
         data.save()
-        print(encoded_password)
-        return render(request, 'login.html', {})
+        return HttpResponseRedirect(reverse('login'))
    
     return render(request, 'register.html', {})
 
@@ -35,16 +34,9 @@ def register(request):
 def login(request):
     if request.method == 'POST':
         student_id = request.POST['username']
-        encoded_password = base64.b64encode(b'{request.POST["password"]}')
-        print(encoded_password)
-        print(student_id)
-        print(request.POST["password"])
-        if Student.objects.filter(pk = student_id).exists():
-            if Student.objects.filter(student_password = encoded_password).exists():
-                print ('password matched')
-                return render(request, 'chatbot.html')
-            else:
-                return HttpResponse('wrong password')   
+        password = request.POST['password']
+        if Student.objects.filter(student_id = student_id, student_password = password).exists():
+            return HttpResponseRedirect(reverse('chatbot'))  
         else:
             return HttpResponse('Wrong ID number')
     return render(request, 'login.html', {})
